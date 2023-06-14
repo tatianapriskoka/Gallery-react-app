8af37d5a2f50b08d17404407a337230710e2ed6d
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-06-14 20:54:22 +0300

    feat: components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { WebGallery } from "./Gallery"
 
 
 function App() {
@@ -5,7 +6,38 @@ function App() {
 
   return (
     <>
-      Soon will be here...
+      <WebGallery
+        photos={[
+          {
+            id: 1, src: '/photos/foto1.jpg', preview: '/photos/preview/foto1.jpg',
+            description: ' retro volga car'
+          },
+          {
+            id: 2, src: '/photos/foto2.jpg', preview: '/photos/preview/foto2.jpg',
+            description: ' ducks in the lake'
+          },
+          {
+            id: 3, src: '/photos/foto3.jpg', preview: '/photos/preview/foto3.jpg',
+            description: ' retro volga car'
+          },
+          {
+            id: 4, src: '/photos/foto4.jpg', preview: '/photos/preview/foto4.jpg',
+            description: ' retro volga car'
+          },
+          {
+            id: 5, src: '/photos/foto5.jpg', preview: '/photos/preview/foto5.jpg',
+            description: ' retro volga car'
+          },
+          {
+            id: 6, src: '/photos/foto6.jpg', preview: '/photos/preview/foto6.jpg',
+            description: ' retro volga car'
+          },
+          {
+            id: 7, src: '/photos/foto7.jpg', preview: '/photos/preview/foto7.jpg',
+            description: ' retro volga car'
+          },
+        ]}
+      />
     </>
   )
 }
--- src/Gallery/index.tsx
+++ src/Gallery/index.tsx
@@ -1,14 +1,50 @@
+import { Photo } from "./types";
+import React, { useState } from 'react';
+
+import style from './index.module.scss';
 
 interface WebGalleryProps {
-
+    photos: Photo[],
 }
 export const WebGallery: React.FC<WebGalleryProps> = ({
-    photos,
+    photos
 }) => {
     if (!photos.length) {
         return null;
     }
+
+    const [indexActivePhoto, setIndexActivePhoto] = useState(0);
+    const activePhoto = photos[indexActivePhoto];
+    const prevPhoto = photos[indexActivePhoto - 1];
+    const nextPhoto = photos[indexActivePhoto + 1];
     return (
-        <div>My Gallery</div>
+        <div className={style.Gallery}>
+            <div className={style.GalleryContainer}>
+                <MainPhoto
+                    prevPhoto={prevPhoto}
+                    currentPhoto={currentPhoto}
+                    nextPhoto={nextPhoto}
+                    className={style.GalleryMainPhoto}
+                />
+
+                <Navigation
+                    className={style.GalleryNavigation}
+                    disabledPrev={!prevPhoto}
+                    disabledNext={!nextPhoto}
+                    onPrevClick={() => {
+                        setIndexActivePhoto(indexActivePhoto - 1);
+                    }}
+                    onNextClick={() => {
+                        setIndexActivePhoto(indexActivePhoto + 1);
+                    }}
+                />
+            </div>
+
+            <PreviewGallery
+                activePhotoIndex={indexActivePhoto}
+                photos={photos}
+                className={style.GalleryPreviewList}
+            />
+        </div>
     )
 }
